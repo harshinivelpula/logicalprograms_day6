f6801c488de7901766cf7dfbc59b560da3e408c1
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-17 20:54:03 +0530

    displayfibonacciseries

---
+++ fibonacci.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace logicalprograms_day6
+{
+    public static class fibonacciseries
+    {
+        public static void displayfibonacciseries()
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.WriteLine("Enter the number of elements");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n1 + " " + n2 + " ");
+            for (i = 2; i < number; ++i)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
