b37022b744c558a00da127475a0f937724bab061
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-17 20:41:40 +0530

    greeting message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace logicalprograms_day6
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to logical programs");
+        }
+    }
+}
