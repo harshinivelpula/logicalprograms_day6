02bed2e25f868c6e105933d2b8747afcd3c2114f
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-17 21:00:07 +0530

    checkperfectnumber

---
+++ perfectnumber.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace logicalprograms_day6
+{
+    public static class perfectnumber
+    {
+        public static void checkperfectnumber()
+        {
+            int n, i, sum;
+            int mn, mx;
+            {
+                Console.WriteLine("find the perfect number within the given number of range");
+                Console.WriteLine("input the starting range of number");
+                mn = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("input the starting range of number");
+                mx = Convert.ToInt32(Console.ReadLine());
+
+                for (n = mn; n <= mx; n++)
+                {
+                    i = 1;
+                    sum = 0;
+                    while (i < n)
+                    {
+                        if (n % i == 0)
+                            sum = sum + i;
+                        i++;
+                    }
+                    if (sum == 0)
+                        Console.WriteLine(n);
+                }
+            }
+        }
+    }
+}
