a401c6d0aafee3d9977e3e095f29c383a9a3b3bc
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-18 09:06:53 +0530

    display

---
+++ reverseanumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicalprograms_day6
+{
+    public static class reversenumber
+    {
+        public static void display()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+            while(n != 0)
+                {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+
+            }
+            Console.WriteLine("reversed number is " + reverse);
+        }
+    }
+}
